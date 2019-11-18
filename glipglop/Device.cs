c920e163bc55ff8aa031ee158945814b8a82472b
River Marks <rmarks6767@gmail.com>
2019-11-18 15:13:24 -0500

    Created the device and device manager classes, now to add the serial port reading

---
+++ glipglop/Device.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace glipglop
+{
+    public class Device : SerialPort
+    {
+        private bool pressed;
+        public bool isPressed() => pressed;
+        public bool isReleased() => !pressed;
+
+        public Device(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+            : base(portName, baudRate, parity, dataBits, stopBits)
+        {
+            pressed = false;
+        }
+    }
+}
---
+++ glipglop/DeviceManager.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace glipglop
+{
+    public class DeviceManager
+    {
+
+        List<SerialPort> devices;
+
+        public DeviceManager()
+        {
+
+        }
+
+        public void ReadData(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (sender is Device port)
+            {
+            }
+        }
+
+        public Device CreateConnection(string portName)
+        {
+            Device device = new Device(portName, 9600, Parity.None, 8, StopBits.One);
+            device.DataReceived += new SerialDataReceivedEventHandler(ReadData);
+            device.Open();
+            return device;
+        }
+
+        public void CloseConnection(Device device) => device.Close();
+
+        public void Pressed(Device device)
+        {
+
+        }
+
+        public void Released()
+        {
+
+        }
+    }
+}
